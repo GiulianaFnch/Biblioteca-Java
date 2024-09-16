ca7d9743d73f9ac9202f32440fb480221ef3f493
giulianafnch <87533967+GiulianaFnch@users.noreply.github.com>
2024-09-16 15:58:14 +0100

    Add files via upload

---
+++ GestaoHorsed/src/gestaohorsed/GestaoHorsed.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestaohorsed;
+
+/**
+ *
+ * @author giuli
+ */
+public class GestaoHorsed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // código para abrir o form principal
+        fPrincipal frm=new fPrincipal();
+        frm.setVisible(true);
+    }
+    
+}
